1d96e9699004c4364ddef8ef485daa562b0ea004
Anthony Githaka <agithaka@live.com>
2022-02-12 08:06:03 +0300

    Genesis

---
+++ js/destructuring_objects.js
@@ -0,0 +1,11 @@
+const personalInfo = {
+    firstName: 'Anthony',
+    lastName: 'Githaka',
+    town: 'Molo',
+    nationality: 'Kenyan',
+    county: 'Nakuru',
+};
+
+const {firstName,county} = personalInfo;
+console.
+log(`${firstName} ${county}`); // backticks template literals
---
+++ js/template_literals.js
@@ -0,0 +1,8 @@
+let word1 = 'First';
+let word2 = 'Second';
+let word3 = 'Third';
+let word4 = 'Forth';
+
+const sentence = `${word1} ${word2} ${word3} ${word4}`;
+
+console.log(sentence);
